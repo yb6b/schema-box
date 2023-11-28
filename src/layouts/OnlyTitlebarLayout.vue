e80b0a1e10adfd9112472706e1d62e6c6a203d3c
yb6b <seetxx@qq.com>
2023-11-28 16:29:31 +0800

    refac: delete components/racer

---
+++ src/layouts/OnlyTitlebarLayout.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import LoadFiles from 'components/racer/LoadFiles.vue'
+import { titleRef } from 'src/libs/utils'
+
+const p = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+})
+
+titleRef.value = p.title
+</script>
+
+<template>
+  <QLayout view="hHh lpR fFf" class="bg-blue-grey-1">
+    <QHeader reveal elevated class="bg-grey-1 text-dark">
+      <QToolbar>
+        <QToolbarTitle>
+          <QBtn to="/" flat square title="形码盒子主页">
+            <QAvatar square>
+              <img src="~assets/icon.svg">
+            </QAvatar>
+          </QBtn>
+          <span style="letter-spacing: 2px;" class="text-blue-grey-9">
+            {{ title }}
+          </span>
+          <slot name="toolbar" />
+        </QToolbarTitle>
+      </QToolbar>
+    </QHeader>
+
+    <QPageContainer>
+      <div class="flex flex-center">
+        <slot />
+      </div>
+    </QPageContainer>
+  </QLayout>
+</template>
--- src/pages/racer/RacerPage.vue
+++ src/pages/racer/RacerPage.vue
@@ -1,31 +1,143 @@
 <script lang="ts" setup>
-import LoadFiles from 'components/racer/LoadFiles.vue'
-import { titleRef } from 'src/libs/utils'
+import type { SchemaDict } from 'libs/schema'
 
-titleRef.value = '测评工具'
+import { reactive, ref } from 'vue'
+import { mdiTextBoxEditOutline } from '@quasar/extras/mdi-v7'
+import { countDictItems, createEmptySchema } from 'libs/schema/schemaUtils'
+import { loadDuoduoDict } from 'libs/platforms/duoduo/load'
+
+import OnlyTitlebarLayout from 'layouts/OnlyTitlebarLayout.vue'
+import LoadFileDialog from 'components/loadFile/LoadFileDialog.vue'
+import VisualizeDialog from './visualize/VisualizeDialog.vue'
+
+const openArticle = ref(false)
+const articleData = reactive({
+  name: '',
+  content: '',
+})
+
+const openSchema = ref(false)
+const schemaData = ref(createEmptySchema())
+
+const openVisualize = ref(false)
+
+function displayDict(d: SchemaDict): string {
+  let r = ''
+  let count = 0
+  // 最多前10条
+  for (const i of d.items) {
+    r += `${i[1]} ${i[0]} `
+    if (++count >= 10)
+      break
+  }
+  return r
+}
 </script>
 
 <template>
-  <QLayout view="hHh lpR fFf" class="bg-blue-grey-1">
-    <QHeader reveal elevated class="bg-grey-1 text-dark">
-      <QToolbar>
-        <QToolbarTitle>
-          <QBtn to="/" flat square title="形码盒子主页">
-            <QAvatar square>
-              <img src="~assets/icon.svg">
-            </QAvatar>
-          </QBtn>
-          <span style="letter-spacing: 2px;" class="text-blue-grey-9">
-            测评工具
-          </span>
-        </QToolbarTitle>
-      </QToolbar>
-    </QHeader>
+  <OnlyTitlebarLayout title="测评工具">
+    <div class="columns q-pt-xl">
+      <QList bordered padding style="width:22rem" class="col bg-white">
+        <QItemLabel header>
+          赛文
+        </QItemLabel>
+        <QItem clickable @click="openArticle = true">
+          <QItemSection v-if="articleData.content === ''">
+            <QItemLabel>
+              <div class="text-subtitle1 text-primary">
+                点击加载赛文
+              </div>
+            </QItemLabel>
+          </QItemSection>
+          <QItemSection v-else>
+            <QItemLabel>
+              {{ articleData.name }}
+              <QBadge
+                rounded
+                color="blue-grey-5"
+                :label="`${articleData.content.length} 字`"
+              />
+            </QItemLabel>
+            <QItemLabel caption style="white-space: nowrap;overflow: hidden;text-overflow: ellipsis;">
+              {{ articleData.content.slice(0, 50) }}
+            </QItemLabel>
+          </QItemSection>
 
-    <QPageContainer>
-      <div class="row justify-center">
-        <LoadFiles />
+          <QItemSection side>
+            <QIcon :name="mdiTextBoxEditOutline" />
+          </QItemSection>
+        </QItem>
+        <QSeparator spaced />
+        <QItemLabel header>
+          码表
+        </QItemLabel>
+        <QItem clickable @click="openSchema = true">
+          <QItemSection v-if="countDictItems(schemaData) === 0">
+            <QItemLabel>
+              <div class="text-subtitle1 text-primary">
+                点击载入码表
+              </div>
+            </QItemLabel>
+          </QItemSection>
+          <QItemSection v-else>
+            <QItemLabel>
+              {{ schemaData?.cfg?.name || "" }}
+              <QBadge
+                rounded
+                color="blue-grey-5"
+                :label="`${countDictItems(schemaData)} 行`"
+              />
+            </QItemLabel>
+            <QItemLabel caption style="white-space: nowrap;overflow: hidden;text-overflow: ellipsis;">
+              {{ displayDict(schemaData.dicts[0]) }}
+            </QItemLabel>
+          </QItemSection>
+
+          <QItemSection side>
+            <QIcon :name="mdiTextBoxEditOutline" />
+          </QItemSection>
+        </QItem>
+      </QList>
+
+      <LoadFileDialog
+        v-model:dialog="openSchema"
+        dict-mode
+        @value="(v) => {
+          schemaData.cfg = v.options
+          loadDuoduoDict(v.content!).then(
+            v => schemaData.dicts[0] = v.dicts[0],
+          )
+        }"
+      />
+      <LoadFileDialog
+        v-model:dialog="openArticle"
+        @value="v => {
+          v.content?.getText().then(t => articleData.content = t)
+          articleData.name = v.options.name!
+        }"
+      />
+
+      <VisualizeDialog
+        v-if="openVisualize"
+        v-model:dialog="openVisualize"
+        :article="articleData"
+        :dict="schemaData"
+      />
+
+      <div class="col row flex-center">
+        <QBtn
+          :disable="!articleData.name || !schemaData?.cfg?.name"
+          label="计 算"
+          color="primary"
+          rounded
+          glossy
+          class="col-6 q-mt-lg"
+          @click="
+            () => {
+              openVisualize = true
+            }"
+        />
       </div>
-    </QPageContainer>
-  </QLayout>
+    </div>
+  </OnlyTitlebarLayout>
 </template>
